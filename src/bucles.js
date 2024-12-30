531686fd7142c48a6a44770778a5ffa2bdbf1739
servo pc <fernandonando98@live.com.mx>
2024-12-29 19:29:02 -0600

    separar archivos y mejorar descirpcion

---
+++ src/bucles.js
@@ -0,0 +1,60 @@
+
+// 2.- BUCLES
+
+/**
+ * Ejercicio 2.1 
+ * 
+ * Haz una función llamada count que reciba un número n como parámetro, y retorne una cadena que contenga todos los números enteros desde el 1 hasta n separados por comas
+ */
+
+// count(number)
+
+/**
+ * Ejercicio 2.2
+ * 
+ * Haz una función de nombre sumUntil que reciba un número n como parámetro y que retorne la suma de todos los números desde 1 hasta n.
+ */
+
+// sumUntil(number)
+
+/** 
+ * Ejercicio 2.3
+ * 
+ * Haz una funcion llamada getPairs que reciba un número n como parámetro y retorne una cadena con todos los números desde 1 hasta n que sean pares separados por coma. si puedes reutiliza la funcion isPair
+*/
+
+// getPairs(number)
+
+// CADENAS (STRINGS)
+
+/**
+ * Ejercicio 3.1
+ * 
+ * Haz una función llamada vocalsCount que reciba una cadena como parámetro y regrese cuántas vocales contiene tanto mayúsculas como minúsculas
+ */
+
+// vocalsCount(string)
+
+
+/** 
+ * Ejercicio 3.2
+ * 
+ * Haz una función llamada countWords que reciba una frase como parámetro y regrese un número que indique cuántas palabras hay en la frase
+ */
+
+// countWords(phrase)
+
+/**
+ * Ejercicio 3.3
+ * 
+ * Haz una función llamada hideInfo que reciba una cadena como parámetro y regrese una cadena la misma cadena pero remplazando todos los caracteres por * execpto los últimos 4 dígitos. Por ejemplo, "1234567890" -> "******7890"
+ */
+
+/**
+ * Ejercicio 3.4
+ * 
+ * Haz una función llamada getlastWord que reciba una cadena como parámetro y regrese la última palabra
+ * 
+ * getlastWord(string)
+ * 
+ */
