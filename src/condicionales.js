6480213a14ff45557d3483a9e5088990a47f8a54
AilynMza <ailynmorales501@gmail.com>
2024-12-29 21:22:37 -0600

    feat: respuesta ejercicio 1.5 (usando else if y rangos)

--- src/condicionales.js
+++ src/condicionales.js
@@ -85,5 +85,16 @@ switch (number){
  * gradeStudent(number)
  */
 function gradeStudent(number){
-
+if (number >= 90  && number <=100){
+    return 'A'
+} else if(number >= 80 && number <= 89){
+    return 'B' 
+} else if(number >= 70 && number <= 79){
+    return 'C'
+} else if(number >= 60 && number <= 69){
+    return 'D'
+} else if(number >= 0 && number <= 59){
+    return 'F'
+} 
+return 'Número inválido'
 }
